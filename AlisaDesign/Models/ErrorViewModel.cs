3b6563135ad1e70ec5739d4957904da655ee6f1c
Disorol <zzDisorolAnimationzz@gmail.com>
2023-09-11 11:34:57 +0500

    Добавьте файлы проекта.

---
+++ AlisaDesign/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlisaDesign.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
