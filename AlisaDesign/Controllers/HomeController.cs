f2a17b0187e94095f21934a162aae35f4b87d244
alisa <gggg_gggg_21@mail.ru>
2023-09-14 13:48:27 +0500

    Gallery + Service

--- AlisaDesign/Controllers/HomeController.cs
+++ AlisaDesign/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace AlisaDesign.Controllers
         {
             return View();
         }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
