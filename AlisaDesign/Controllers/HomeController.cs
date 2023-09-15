bdc0e1ca45e426f566e6e76b4ecd43352a65157f
alisa <gggg_gggg_21@mail.ru>
2023-09-15 10:08:52 +0500

    new view About

--- AlisaDesign/Controllers/HomeController.cs
+++ AlisaDesign/Controllers/HomeController.cs
@@ -32,6 +32,11 @@ namespace AlisaDesign.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
